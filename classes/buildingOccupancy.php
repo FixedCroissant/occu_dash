407eddfc55982e880dd7dbee71bd5f4402e3eaf9
Joshua Williams <jjwill10@ncsu.edu>
2015-08-31 10:15:20 -0400

    Initial Commit

---
+++ classes/buildingOccupancy.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jjwill10
+ * Date: 8/31/2015
+ * Time: 10:11 AM
+ * Description:
+ */
+
+class buildingOccupancy {
+
+
+
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jjwill10
+ * Date: 8/31/2015
+ * Time: 10:10 AM
+ */
+
