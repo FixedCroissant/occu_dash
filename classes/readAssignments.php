2805749936c01002b155d7ed9338bc10ef725875
Joshua Williams <jjwill10@ncsu.edu>
2015-09-22 08:57:38 -0400

    new class (readAssignments.php)  to read data and blank sample file to utilize that said class

---
+++ classes/readAssignments.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jjwill10
+ * Date: 9/22/2015
+ * Time: 8:47 AM
+ */
+
+class readAssignments {
+
+
+
+
+
+}
---
+++ index_READ_DATA.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jjwill10
+ * Date: 9/22/2015
+ * Time: 8:48 AM
+ */
+
